6ff712a6f99fdab08f78fd429058c7464277aaee
Rajath Sathyaprakash <79147737+raj1294@users.noreply.github.com>
2026-08-16 21:42:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-conda create --name XcovSpec python==3.10 -c https://cxc.cfa.harvard.edu/conda/ciao ciao caldb_main
+conda create --name xcovspec python==3.10
+conda install --name xcovspec -c https://cxc.cfa.harvard.edu/conda/ciao ciao caldb_main
 #conda install —name XcovSpec heasoft
-source activate XcovSpec
+source activate xcovspec
+python -m pip install astropy scipy scikit-learn stingray numpy==2.0.0 numba==0.67.0 cython wget astromy-ds9
 python -m pip install https://www.astro.rug.nl/software/kapteyn/kapteyn-3.4.tar.gz
-python -m pip install astropy scipy scikit-learn stingray numpy==2.0.0 numba==0.67.0 cython 
 export PYTHONPATH=/opt/homebrew/lib/python3.10/site-packages:${PYTHONPATH}
