b1d57ac12e4a2b1aa18a8fde8c133b7032d43791
Rajath Sathyaprakash <79147737+raj1294@users.noreply.github.com>
2026-08-17 01:49:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@ conda create --name xcovspec python==3.10
 conda install --name xcovspec -c https://cxc.cfa.harvard.edu/conda/ciao ciao caldb_main
 #conda install —name XcovSpec heasoft
 source activate xcovspec
-python -m pip install astropy scipy scikit-learn stingray numpy==2.0.0 numba==0.67.0 cython wget astromy-ds9 astroquery
-python -m pip install https://www.astro.rug.nl/software/kapteyn/kapteyn-3.4.tar.gz
-export PYTHONPATH=/opt/homebrew/lib/python3.10/site-packages:${PYTHONPATH}
+python -m pip install -q astropy scipy scikit-learn stingray numpy==2.0.0 numba==0.67.0 cython wget astromy-ds9 astroquery
+cd /Users/raj1294/anaconda3/envs/xcovspec/lib/python3.10/site-packages
+wget https://www.astro.rug.nl/software/kapteyn/kapteyn-3.4.tar.gz
+tar -xvf kapteyn-3.4.tar.gz
+rm -f kapteyn-3.4.tar.gz
+cd kapteyn-3.4
+export CFLAGS="-Wno-error=int-conversion"
+python setup.py install
+cd ~/software/XcovSpec/
+source deactivate
