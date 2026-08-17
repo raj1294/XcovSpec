1392cbf64d8f546fb05c8b6ed1426a371dc0a1af
Rajath Sathyaprakash <79147737+raj1294@users.noreply.github.com>
2026-08-17 03:22:08 +0100

    Remove quiet flag from pip install command

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 conda create --name xcovspec python==3.10
 conda install --name xcovspec -c https://cxc.cfa.harvard.edu/conda/ciao -c conda-forge ciao caldb_main ciao-contrib sherpa ds9
 source activate xcovspec
-python -m pip install -q astropy scipy scikit-learn stingray numpy==2.0.0 numba==0.67.0 cython wget astroquery
+python -m pip install astropy scipy scikit-learn stingray numpy==2.0.0 numba==0.67.0 cython wget astroquery
 cd /Users/raj1294/anaconda3/envs/xcovspec/lib/python3.10/site-packages
 wget https://www.astro.rug.nl/software/kapteyn/kapteyn-3.4.tar.gz
 tar -xvf kapteyn-3.4.tar.gz
